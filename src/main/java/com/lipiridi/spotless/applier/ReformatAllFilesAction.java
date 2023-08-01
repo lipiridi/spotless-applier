d9a534e939cbd107e7c0860ce6f198939086cb35
Dimitrii Lipiridi <dimitrii.lipiridi@delasport.com>
2023-08-01 22:59:51 +0300

    Add ReformatAllFilesAction action

---
+++ src/main/java/com/lipiridi/spotless/applier/ReformatAllFilesAction.java
@@ -0,0 +1,19 @@
+package com.lipiridi.spotless.applier;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+
+public class ReformatAllFilesAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getProject();
+        if (project == null) {
+            return;
+        }
+
+        new ReformatProcessor(project, null).run();
+    }
+}
+
---
+++ src/main/java/com/lipiridi/spotless/applier/ReformatFileAction.java
@@ -0,0 +1,44 @@
+package com.lipiridi.spotless.applier;
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+
+public class ReformatFileAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent event) {
+        Project project = event.getProject();
+        if (project == null) {
+            return;
+        }
+
+        Optional
+                .ofNullable(event.getDataContext().getData(CommonDataKeys.EDITOR))
+                .map(Editor::getDocument)
+                .ifPresent(document -> new ReformatProcessor(project, document).run());
+    }
+
+    @Override
+    public @NotNull ActionUpdateThread getActionUpdateThread() {
+        return super.getActionUpdateThread();
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent event) {
+        // Check if there is an active editor.
+        boolean isActiveEditorAvailable = Optional
+                .ofNullable(event.getDataContext().getData(CommonDataKeys.EDITOR)).isPresent();
+
+        // Enable the action only when there is an active editor.
+        event.getPresentation().setEnabled(isActiveEditorAvailable);
+        event.getPresentation().setVisible(isActiveEditorAvailable);
+    }
+}
+
